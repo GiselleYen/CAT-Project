53c4bb8263a5a9f3d2a673dcc0b151009af86ffd
Edwineyee0923 <101932323+Edwineyee0923@users.noreply.github.com>
2023-01-24 23:50:17 +0800

    Wei En's version updated6

--- app/src/main/java/bday/gits/Reading.java
+++ app/src/main/java/bday/gits/Reading.java
@@ -1,5 +1,7 @@
 package bday.gits;
 
+import android.content.Intent;
+import android.net.Uri;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.View;
@@ -9,9 +11,24 @@ import android.widget.ScrollView;
 
 public class Reading extends AppCompatActivity {
 
+    LinearLayout funny_video;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_reading);
+        funny_video = (LinearLayout) findViewById(R.id.funny_video);
+        funny_video.setOnClickListener(new View.OnClickListener() {
+
+            @Override
+            public void onClick(View v) {
+                Intent intent = new Intent();
+                intent.setAction(Intent.ACTION_VIEW);
+                intent.addCategory(Intent.CATEGORY_BROWSABLE);
+                intent.setData(Uri.parse("https://www.scienceofpeople.com/funny-videos/"));
+                startActivity(intent);
+            }
+        });
     }
 }
+
